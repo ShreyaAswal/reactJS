49a4e1827a2ff4d786171c004d5f684fc22f8e0f
shr22 <aswalshreya.227@gmail.com>
2024-12-18 00:46:56 +0530

    how to create react projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <h1>Shreya Learning JS</h1>
+    <h2>html</h2>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <h1>Shreya learning react using Vite</h1>
+  )
+}
+
+export default App
