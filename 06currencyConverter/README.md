6aa59f7fed9688bda77f7800f178915557700925
shr22 <aswalshreya.227@gmail.com>
2024-12-27 01:45:30 +0530

    inputBox and custom hook

---
+++ 06currencyConverter/README.md
@@ -0,0 +1,9 @@
+## hooks 
+Hooks let you use different React features from your components. You can either use the built-in Hooks or combine them to build your own.
+some examples of built in hooks are- useCallback, useState, useEffect, useState, useId
+but we can also create our own hooks- hooks are nothing just a method, which return us some data
+
+
+- url => https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json
+
+- remember to always gives keys while using loop in react
---
+++ 06currencyConverter/src/Components/InputBox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function InputBox(label, classname="", Amount, currentCurrency, onAmountChange, onCurrencyChange, currencyOptions=[], amountDisable=false, currencyDisable=false ){
+    return (
+        <>
+            <div className={`bg-white p-3 rounded-lg text-sm flex ${classname}`}>
+                <div className="w-1/2">
+
+                    <label className="text-black/40 mb-2 inline-block"> {label} </label>
+                    <input type="number" className="outline-none w-full bg-transparent py-1.5" 
+                    placeholder="Amount" value={Amount} onChange={ (e)=>onAmountChange && onAmountChange(e.target.value)} disabled={amountDisable}/>
+
+                </div>
+                <div className="w-1/2flex flex-wrap justify-end text-right">
+
+                    <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                    <select  className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                        value={currentCurrency}  onChange={ (e)=>onCurrencyChange && onCurrencyChange(e.target.value)} disabled={currencyDisable} >
+                        { //looping 
+                            currencyOptions.map( (currency)=> (
+                                <option value={currency} key={currency}>{currency}</option> //returning option element to create list
+                            ))
+                        } 
+
+                    </select>
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default InputBox
