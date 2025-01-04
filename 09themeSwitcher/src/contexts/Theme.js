7ae1d03436fcc7577eb2cfe8471b44e5da294937
shr22 <aswalshreya.227@gmail.com>
2025-01-05 00:49:20 +0530

    theme switcher

---
+++ 09themeSwitcher/src/contexts/Theme.js
@@ -0,0 +1,16 @@
+import { createContext,useContext } from "react";
+
+export const ThemeContext = createContext(
+    {
+        themeMode:"light",
+        lightTheme: ()=>{},
+        darkTheme: ()=>{}
+    }
+)
+
+export const ThemeProvider= ThemeContext.Provider
+
+// custom hook
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
